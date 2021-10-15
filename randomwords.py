eb3e5811aa0e4241a5d5655dbdf83bb0256eb4ee
ozzi- <oz@oz-web.com>
2021-10-15 22:01:45 +0200

    refacotoring
    - cleanup
    - requirements pip file

--- randomwords.py
+++ randomwords.py
@@ -1,22 +1,31 @@
-# Get geckodriver -> https://github.com/mozilla/geckodriver/releases
+# 0. https://developer.twitter.com/en/apply-for-access & apply for account
+# 1.	Create App
+# 2.	Make sure App permission is set to "Read and Write"
+# 3.	Create creds and save as creds.py
+# 4.	Get geckodriver binary and place in a path where python will find it
+#		https://github.com/mozilla/geckodriver/releases
+
 import requests, random, tweepy as tw
+from urllib.parse import urljoin, urlparse
 from selenium import webdriver
 from time import sleep
-from urllib.parse import urljoin, urlprase
 from creds import *
+
+aliExpressBaseSearchURL='https://www.aliexpress.com/wholesale?SearchText='
+aliExpressItemHrefMarker='www.aliexpress.com/item/'
+sorryNotFoundMarker="Sorry, your search \""
 
 def getWords():
 	word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-
 	response = requests.get(word_site)
 	WORDS = response.content.splitlines()
 	word1 = str(random.choice(WORDS).decode())
-	word2 = str(random.choice (WORDS).decode())
+	word2 = str(random.choice(WORDS).decode())
 	return word1+"+"+word2
 
 def doSearch(browser,searchText):
 	print("searching for "+searchText)
-	browser.get('https://www.aliexpress.com/wholesale?SearchText='+searchText)
+	browser.get(aliExpressBaseSearchURL+searchText)
 	elem = browser.find_element_by_xpath("//*")
 	return elem.get_attribute("outerHTML")
 
@@ -24,8 +33,9 @@ def getFirstItemHref(browser):
 	elems = browser.find_elements_by_xpath("//a[@href]")
 	for elem in elems:
 		href = elem.get_attribute("href")
-		if "www.aliexpress.com/item/" in href:
+		if aliExpressItemHrefMarker in href:
 			return href
+	raise Exception("Could not find any hrefs starting with /item - my guess its DNS :^)")
 
 def screenShotItem(browser,url):
 	scPath = "screenshot.png";
@@ -38,21 +48,23 @@ def tweetWithImage(text,mediaPath):
 	auth.set_access_token(access_token, access_token_secret)
 	api = tw.API(auth, wait_on_rate_limit=True)
 	sleep(1)
-	browser.quit()
 	api.update_status_with_media(text,mediaPath)
-
+	print("Tweeted "+text)
 
 searchText = getWords()
 browser = webdriver.Firefox()
 responseBody = doSearch(browser,searchText)
 
-while "Sorry, your search \"" in responseBody:
+while sorryNotFoundMarker in responseBody:
 	searchText = getWords()
 	responseBody = doSearch(browser,searchText)
 
-# https://www.aliexpress.com/item/4000288191878.html?algo_pvid=d7a0a9a4-86b4-4d5e-b1c1-f024ebb3a6e3&algo_exp_id=d7a0a9a4-86b4-4d5e-b1c1-f024ebb3a6e3-0&pdp_ext_f=%7B%22sku_id%22%3A%2210000014022551024%22%7D
 href=getFirstItemHref(browser)
 href=urljoin(href, urlparse(href).path) 
 scPath=screenShotItem(browser,href)
 tweetWithImage("\""+searchText.replace("+"," ")+"\"",scPath)
+browser.quit()
 quit()
+
+
+
