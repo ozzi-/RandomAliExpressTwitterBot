4ccc09a5881098b67d292180e92b05115cd27117
ozzi- <oz@oz-web.com>
2021-10-19 09:45:09 +0200

    chromium headless

--- randomwords.py
+++ randomwords.py
@@ -2,18 +2,23 @@
 # 1.	Create App
 # 2.	Make sure App permission is set to "Read and Write"
 # 3.	Create creds and save as creds.py
-# 4.	Get geckodriver binary and place in a path where python will find it
-#		https://github.com/mozilla/geckodriver/releases
+# 4.	Get chromiumdriver binary and place in a path where python will find it
+#		https://sites.google.com/chromium.org/driver/
 
 import requests, random, tweepy as tw
 from urllib.parse import urljoin, urlparse
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 from creds import *
 
 aliExpressBaseSearchURL='https://www.aliexpress.com/wholesale?SearchText='
 aliExpressItemHrefMarker='www.aliexpress.com/item/'
 sorryNotFoundMarker="Sorry, your search \""
+WINDOW_SIZE = "1280,1000"
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 def getWords():
 	word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
@@ -52,7 +57,7 @@ def tweetWithImage(text,mediaPath):
 	print("Tweeted "+text)
 
 searchText = getWords()
-browser = webdriver.Firefox()
+browser = webdriver.Chrome(chrome_options=chrome_options)
 responseBody = doSearch(browser,searchText)
 
 while sorryNotFoundMarker in responseBody:
@@ -65,6 +70,3 @@ scPath=screenShotItem(browser,href)
 tweetWithImage("\""+searchText.replace("+"," ")+"\"",scPath)
 browser.quit()
 quit()
-
-
-
